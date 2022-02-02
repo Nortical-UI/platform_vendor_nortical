2a4cd03f0997b1eb1ca8d13aa910a1fc9ea74885
SakuraiLH <hikarilh@foxmail.com>
2022-02-02 11:25:30 +0800

    nortical: Standarize our overlay
    Signed-off-by: SakuraiLH <hikarilh@foxmail.com>

--- config/overlay.mk
+++ config/overlay.mk
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Vendor overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/nortical/overlay
+# Common overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/nortical/overlay/common
 
